d49fdb0d78b2a9a8e590e8d459aea308ff5b0426
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-06-28 17:00:57 -0300

    adds checking logic for unique items

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,15 @@
 package application;
 
+import entities.LogEntry;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,11 +18,21 @@ public class Main {
         System.out.println("Enter file full path: ");
         String path = sc.nextLine();
 
-        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
+            Set<LogEntry> set = new HashSet<>();
+            String line = br.readLine();
+            while (line != null) {
+                //file format is name[space]date-in-ISO-8601
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+                set.add(new LogEntry(username, moment));
+                line = br.readLine();
+            }
+            System.out.println("Total unique users: " + set.size());
 
-
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
